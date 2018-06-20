156db89bff717974001143c2563b995f4f917c4e
dambergn <dambergn@gmail.com>
2018-06-20 11:01:28 -0700

    cleaned up code.

--- src/main.js
+++ src/main.js
@@ -4,60 +4,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/main.css'
 
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Link
-// } from 'react-router-dom';
-
-class VideoPlayer extends React.Component { // MyCoolApp is the name of the app
+class VideoPlayer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: '',
-    }
+    this.state = {}
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
     this.createVideo = this.createVideo.bind(this);
-    this.videoInfo = this.videoInfo.bind(this);
   }
-  
-
-  // const input = document.querySelector('input[type="file"]');
-
-  // input.addEventListener('change', function (e) {
-  //   e.preventDefault();
-  //   let fileName = input.files[0].name;
-  //   let tmppath = URL.createObjectURL(event.target.files[0]);
-  //   console.log('file info:', input.files[0])
-  
-  //   createVideo(tmppath, fileName);
-  // }, false);
 
   handleChange(event) {
-    this.setState({value: event.target.value});
-    // let path = event.target.value;
-    // console.log('Handle Change:', path);
     let fileName = event.target.files[0].name;
-    console.log('file name:', fileName);
+    // console.log('file name:', fileName);
     let tmppath = URL.createObjectURL(event.target.files[0]);
-    console.log('temp path:', tmppath);
+    // console.log('temp path:', tmppath);
 
     this.createVideo(tmppath, fileName);
   }
 
-  handleSubmit(event) {
-    alert('file selected: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  videoInfo() {
-    let fileName = input.files[0].name;
-    let tmppath = URL.createObjectURL(event.target.files[0]);
-
-    createVideo(tmppath, fileName);
-  }
-  
   createVideo(videoFile) {
     var child = document.getElementById("video-player");
     child.parentNode.removeChild(child);
@@ -83,19 +46,15 @@ class VideoPlayer extends React.Component { // MyCoolApp is the name of the app
     return <div>
       <h1>Video Player</h1>
       <p>Choose a local video video file to play in web browser.</p>
-      {/* <input onClick={videoInfo} type="file" id="video"></input> */}
-
       <form onSubmit={this.handleSubmit}>
         <label>
           <input type="file" value={this.state.value} onChange={this.handleChange} />
         </label>
       </form>
-
-      {/* <input type="file" id="video"></input> */}
       <div id="video-player"></div>
       </div>;
   }
 }
 
 const root = document.getElementById('root');
-ReactDOM.render(<VideoPlayer />, root);  // Class and render need to be the same.
+ReactDOM.render(<VideoPlayer />, root);
